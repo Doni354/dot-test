34e18790c27b9845f47272498d1c37e6214eb1b6
ドニ_354 <110459094+Doni354@users.noreply.github.com>
2026-06-19 22:49:10 +0700

    jwt handling

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,10 +4,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { User } from '../entities/user.entity';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User]),
+    PassportModule,
     JwtModule.register({
       global: true,
       secret: 'secretKey',
@@ -15,7 +18,7 @@ import { User } from '../entities/user.entity';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   exports: [AuthService],
 })
 export class AuthModule {}
