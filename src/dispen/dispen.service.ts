1b470f50044cfd7331e3312d448ff9d41ce56b8f
ドニ_354 <110459094+Doni354@users.noreply.github.com>
2026-06-19 22:02:36 +0700

    service Dispen
    penghubung controller ke model

--- src/dispen/dispen.service.ts
+++ src/dispen/dispen.service.ts
@@ -1,26 +1,52 @@
-import { Injectable } from '@nestjs/common';
-import { CreateDispenDto } from './dto/create-dispen.dto';
-import { UpdateDispenDto } from './dto/update-dispen.dto';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Dispen } from './entities/dispen.entity';
 
 @Injectable()
 export class DispenService {
-  create(createDispenDto: CreateDispenDto) {
-    return 'This action adds a new dispen';
+  constructor(
+    @InjectRepository(Dispen)
+    private dispenRepository: Repository<Dispen>,
+  ) {}
+
+  // add dispen
+  async createDispen(userId: number, alasan: string) {
+    const newDispen = this.dispenRepository.create({
+      id_user: userId,
+      alasan: alasan,
+      status: 'pending', // Default pas pertama kali bikin
+    });
+    return this.dispenRepository.save(newDispen);
   }
 
-  findAll() {
-    return `This action returns all dispen`;
+  // view data
+  async findAll(userPayload: any) {
+    if (userPayload.role === 'admin') {
+      return this.dispenRepository.find();
+    } else {
+      return this.dispenRepository.find({
+        where: { id_user: userPayload.sub },
+      });
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} dispen`;
+  // Admin acc
+  async updateStatus(id: number, status: string) {
+    const dispen = await this.dispenRepository.findOne({ where: { id } });
+    if (!dispen) {
+      throw new NotFoundException(`Data dispen ID ${id} nggak ketemu cuy`);
+    }
+    dispen.status = status;
+    return this.dispenRepository.save(dispen);
   }
 
-  update(id: number, updateDispenDto: UpdateDispenDto) {
-    return `This action updates a #${id} dispen`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} dispen`;
+  // Hapus data dispen
+  async remove(id: number) {
+    const result = await this.dispenRepository.delete(id);
+    if (result.affected === 0) {
+      throw new NotFoundException(`Data dispen ID ${id} nggak ketemu cuy`);
+    }
+    return { message: `Data dispen ID ${id} berhasil dihapus` };
   }
 }
