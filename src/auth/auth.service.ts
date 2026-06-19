f45cb3cf3038b7db7568367e1ad08ba92de60418
ドニ_354 <110459094+Doni354@users.noreply.github.com>
2026-06-19 21:46:38 +0700

    auth service
    service untuk auth login

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,28 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import { User } from '../entities/user.entity';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+    private jwtService: JwtService,
+  ) {}
+
+  async login(email: string, plainPass: string) {
+    const user = await this.userRepository.findOne({ where: { email } });
+
+    if (!user || !(await bcrypt.compare(plainPass, user.pass))) {
+      throw new UnauthorizedException('Email atau password salah cuy');
+    }
+
+    const payload = { sub: user.id, email: user.email, role: user.role };
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
+  }
+}
